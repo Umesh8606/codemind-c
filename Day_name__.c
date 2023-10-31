d4d024692ee769583947ded2ddfc0953d07770f3
Umesh8606 <147487165+Umesh8606@users.noreply.github.com>
2023-10-31 21:25:25 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x==1)     printf("Monday");     else if(x==2)     printf("Tuesday");     else     printf("Invalid input"); }

---
+++ Day_name__.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x==1)
+    printf("Monday");
+    else if(x==2)
+    printf("Tuesday");
+    else 
+    printf("Invalid input");
+}
